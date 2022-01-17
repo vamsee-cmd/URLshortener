aeda02c9a2709d9792b836eb4e7dcbc7bde72f11
vamsee-cmd <vamshi.embadi1234@gmail.com>
2022-01-17 23:11:51 +0530

    remote sql added

--- add/index.php
+++ add/index.php
@@ -1,5 +1,5 @@
 <?php 
-$conn = new mysqli('localhost', 'root', '', 'urlshortner');
+$conn = new mysqli('remotemysql.com', 'WK1eggxcgK', 'Q7gkaeCv2k', 'WK1eggxcgK');
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
