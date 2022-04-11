e5c994474f932b435cb529de78346a176a9ab77e
vamsee-cmd <81890751+vamsee-cmd@users.noreply.github.com>
2022-04-11 15:06:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-$conn = new mysqli('remotemysql.com', 'WK1eggxcgK', 'Q7gkaeCv2k', 'WK1eggxcgK');
+$conn = new mysqli('remotemysql.com', 'WK1eggxcgK', 'z9HcRqhc5i', 'WK1eggxcgK');
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
